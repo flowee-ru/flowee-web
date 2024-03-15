ecb2287be17952304ef54d79a6f2f837a10501b1
Kirill <misterkirill666@gmail.com>
2024-03-15 20:17:17 +0500

    feat: add Roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
 import "./globals.css";
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/900.css';
+
 import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
